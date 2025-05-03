ea80e7a5b720534c05511e2052a96b2b9d19e153
Ravel-RP <ravelrodriguesp@gmail.com>
2025-05-03 10:23:17 -0300

    Estudo 03/05

---
+++ admin/config/config.php
@@ -0,0 +1,9 @@
+<?php
+header('Content-Type: charset=utf-8');
+session_start();
+$_SESSION['userLogged'] = 1;
+define("TITLE", "Painel Administrativo v1");
+
+
+
+?>
---
+++ admin/pages/home.php
@@ -0,0 +1,5 @@
+<div class="home_content">
+    <h1>
+        Seja bem vindo, <b></b> <?php echo TITLE; ?>
+    </h1>
+</div>
