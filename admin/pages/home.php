fc11df54024f9fb8c07a9d169fcbd23a089f866b
Ravel-RP <ravelrodriguesp@gmail.com>
2025-05-04 00:52:30 -0300

    att

--- admin/pages/home.php
+++ admin/pages/home.php
@@ -1,5 +1,5 @@
 <div class="home_content">
     <h1>
-        Seja bem vindo, <b> <?php $_SESSION['userName'] ?> </b> <?php echo TITLE; ?>
+        Seja bem vindo, <b> <?= $_SESSION['userName']; ?> </b> <?php echo TITLE; ?>
     </h1>
 </div>
